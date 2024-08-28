fc03a493443334e2d174737b35f98125e17cbbe1
hannesblaman <hannes@blaman.com>
2024-08-28 13:04:48 +0200

    Genomgång funktioner

--- SY22_PRRPRR02_03_Repetition_3/Program.cs
+++ SY22_PRRPRR02_03_Repetition_3/Program.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace SY22_PRRPRR02_03_Repetition_3 {
 	internal class Program {
+		static void HelloWorld() {
+			Console.WriteLine("Hello world");
+		}
+
+		static void Greet(string name) {
+			string greeting = GetGreeting(name);
+
+			Console.WriteLine(greeting);
+		}
+
+		static int Add(int a, int b, int c) {
+			return a + b + c;
+		}
+
+		static string GetGreeting(string name) {
+			return "Hello " + name;
+		}
+
 		static void Main(string[] args) {
+			HelloWorld();
+
+			Greet("Greta");
+
+			string greeting = GetGreeting("Hugo");
+
+			Console.WriteLine(greeting);
+
+			Console.WriteLine(Add(1, 2, 3));
 		}
 	}
 }
